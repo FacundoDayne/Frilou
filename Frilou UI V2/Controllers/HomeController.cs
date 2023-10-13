a4e5a3eedaf901360fcc30b98e70e3e197671093
EdiHakdog <princillo.241270@globalcity.sti.edu.ph>
2023-10-13 08:34:37 +0800

    With admin dash

--- Frilou UI V2/Controllers/HomeController.cs
+++ Frilou UI V2/Controllers/HomeController.cs
@@ -123,6 +123,10 @@ namespace Frilou_UI_V2.Controllers
         {
             return View();
         }
+        public IActionResult HomepageAdmin()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
